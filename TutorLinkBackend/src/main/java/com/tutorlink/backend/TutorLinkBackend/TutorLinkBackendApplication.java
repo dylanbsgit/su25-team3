0f2cabb9541c5129570e79abfbd7268ef2c31015
Terrance Lee <t_lee5@uncg.edu>
2025-06-04 05:29:19 -0400

    Generate initial backend API

---
+++ TutorLinkBackend/src/main/java/com/tutorlink/backend/TutorLinkBackend/TutorLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tutorlink.backend.TutorLinkBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorLinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorLinkBackendApplication.class, args);
+	}
+
+}
